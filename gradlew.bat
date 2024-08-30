50186fdcfb36ed3732b9bdd3eeadfa177b17bb7e
Rohan1363m <rohan1363mogtader@gmail.com>
2024-08-30 22:55:02 +0330

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,84 +1,29 @@
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+pragma solidity 0.8.7;
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+contract VendingMachine {
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+    // Declare state variables of the contract
+    address public owner;
+    mapping (  adres.         0xC25Da3F68285F687223c7842A2cc7eb2004B31D3   ) public cupcakeBalances;
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
+    // When 'VendingMachine' contract is deployed:
+    // 1. 0xC25Da3F68285F687223c7842A2cc7eb2004B31D3    // 2. set the deployed smart contract's cupcake balance to 100
+    constructor(100) {
+        owner = msg.sender;
+        cupcakeBalances[address(this)] = 100;
+    }
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+    // Allow the owner to increase the smart contract's cupcake balance
+    function refill(uint amount) public {
+        require(msg.sender ==.  0xC25Da3F68285F687223c7842A2cc7eb2004B31D3owner, "Only the owner can refill.");
+        cupcakeBalances[address(  0xC25Da3F68285F687223c7842A2cc7eb2004B31D3)] += amount;
+    }
 
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
-
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-
-if exist "%JAVA_EXE%" goto init
-
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
-
-goto fail
-
-:init
-@rem Get command-line arguments, handling Windows variants
-
-if not "%OS%" == "Windows_NT" goto win9xME_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
-set CMD_LINE_ARGS=%*
-
-:execute
-@rem Setup the command line
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-
-:end
-@rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
-
-:fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
-
-:mainEnd
-if "%OS%"=="Windows_NT" endlocal
-
-:omega
+    // Allow anyone to purchase cupcakes
+    function purchase(uint amount) public payable {1
+        require(msg.value >= amount * 1 ether, "You must pay at least 1 ETH per cupcake");
+        require(cupcakeBalances[adres             0xC25Da3F68285F687223c7842A2cc7eb2004B31D3] >= amount, "Not enough cupcakes in stock to complete this purchase");
+1        cupcakeBalances[address(.     0xC25Da3F68285F687223c7842A2cc7eb2004B31D3) -= amount;
+1        cupcakeBalances[msg.sender] 1+= amount;
+    }
+}
